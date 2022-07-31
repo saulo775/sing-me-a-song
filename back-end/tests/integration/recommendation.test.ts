bfb59a204b3b811dc124248a7eb70965b39e2dec
saulo775 <59702227+saulo775@users.noreply.github.com>
2022-07-30 22:02:49 -0300

    TESTS - Implementando factories

--- back-end/tests/integration/recommendation.test.ts
+++ back-end/tests/integration/recommendation.test.ts
@@ -1,13 +1,10 @@
-import { recommendationRepository } from '../../src/repositories/recommendationRepository';
+import recommendationRepository from '../../src/repositories/recommendationRepository.js';
 import supertest from "supertest";
-import { faker } from "@faker-js/faker";
 import app from "../../src/app";
 import { prisma } from "../../src/database";
+import recommendationFactory from '../factories/recomendationFactory.js';
 
-const NAME = faker.name.jobTitle();
-const YOUTUBE_LINK = 'https://www.youtube.com/watch?v=XhUWLTNeNPs&t=37s';
-const RECOMMENDATION = { name: NAME, youtubeLink: YOUTUBE_LINK }
-const AMOUNT = faker.random.numeric(1);
+
 
 
 describe("recommendation test suite", () => {
@@ -16,18 +13,23 @@ describe("recommendation test suite", () => {
     });
 
     it("given name and youtube link, create recommendation", async () => {
-        const response = await supertest(app).post("/recommendations/").send(RECOMMENDATION);
+        const recomendation = recommendationFactory.createDataRecomendation();
+        const response = await recommendationFactory.saveRecommendation(recomendation);
         expect(response.statusCode).toBe(201);
     });
 
     it("given name and youtube link, do not create if a recommendation with the same name already exists", async () => {
-        await supertest(app).post("/recommendations/").send(RECOMMENDATION);
-        const response = await supertest(app).post("/recommendations/").send(RECOMMENDATION);
+        const recomendation = recommendationFactory.createDataRecomendation();
+
+        await recommendationFactory.saveRecommendation(recomendation);
+        const response = await recommendationFactory.saveRecommendation(recomendation);
         expect(response.statusCode).toBe(409);
     });
 
     it("should be able, get first ten recommendations", async () => {
-        await supertest(app).post("/recommendations/").send(RECOMMENDATION);
+        const recomendation = recommendationFactory.createDataRecomendation();
+
+        await recommendationFactory.saveRecommendation(recomendation);
         const response = await supertest(app).get("/recommendations/");
 
         const recommendations = response.body.recommendations
@@ -35,7 +37,9 @@ describe("recommendation test suite", () => {
     });
 
     it("should be able, get random recommendations", async () => {
-        await supertest(app).post("/recommendations/").send(RECOMMENDATION);
+        const recomendation = recommendationFactory.createDataRecomendation();
+
+        await recommendationFactory.saveRecommendation(recomendation);
         const response = await supertest(app).get("/recommendations/random");
         const recommendations = response.body.recommendations;
 
@@ -43,16 +47,20 @@ describe("recommendation test suite", () => {
     });
 
     it("given amount, take this amount recommendations", async () => {
-        await supertest(app).post("/recommendations/").send(RECOMMENDATION);
-        const response = await supertest(app).get(`/recommendations/top/${AMOUNT}`);
+        const recomendation = recommendationFactory.createDataRecomendation();
+
+        await recommendationFactory.saveRecommendation(recomendation);
+        const response = await supertest(app).get(`/recommendations/top/${recomendation.name}`);
         const recommendations = response.body.recommendations;
 
         expect(recommendations).not.toBeNull();
     });
 
     it("given id, take this especific recommendation", async () => {
-        await supertest(app).post("/recommendations/").send(RECOMMENDATION);
-        const rec = await recommendationRepository.findByName(NAME);
+        const recomendation = recommendationFactory.createDataRecomendation();
+
+        await recommendationFactory.saveRecommendation(recomendation);
+        const rec = await recommendationFactory.findRecommendationByName(recomendation);
 
         const response = await supertest(app).get(`/recommendations/${rec.id}`);
         const recommendation = response.body.recommendations;
@@ -61,17 +69,20 @@ describe("recommendation test suite", () => {
     });
 
     it("should be able to upvote a link by its id", async () => {
-        await supertest(app).post("/recommendations/").send(RECOMMENDATION);
-        const rec = await recommendationRepository.findByName(NAME);
+        const recomendation = recommendationFactory.createDataRecomendation();
+
+        await recommendationFactory.saveRecommendation(recomendation);
+        const rec = await recommendationFactory.findRecommendationByName(recomendation);
 
         const response = await supertest(app).post(`/recommendations/${rec.id}/upvote`);
-
         expect(response.statusCode).toBe(200);
     });
 
     it("should be able to upvote a link by its id", async () => {
-        await supertest(app).post("/recommendations/").send(RECOMMENDATION);
-        const rec = await recommendationRepository.findByName(NAME);
+        const recomendation = recommendationFactory.createDataRecomendation();
+
+        await recommendationFactory.saveRecommendation(recomendation);
+        const rec = await recommendationFactory.findRecommendationByName(recomendation);
 
         const response = await supertest(app).post(`/recommendations/${rec.id}/downvote`);
 
